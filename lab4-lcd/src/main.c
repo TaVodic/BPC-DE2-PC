c50198a18dd1c10db05b96c65b9e93a632965c38
Martin FEEC <240703@vutbr.cz>
2023-10-18 13:57:52 +0200

    lab4 mandatory done

--- lab4-lcd/src/main.c
+++ lab4-lcd/src/main.c
@@ -34,6 +34,8 @@
 #include <lcd.h>            // Peter Fleury's LCD library
 #include <stdlib.h>         // C library. Needed for number conversions
 
+#define N_CHARS 1
+
 
 /* Function definitions ----------------------------------------------*/
 /**********************************************************************
@@ -45,15 +47,41 @@
 int main(void)
 {
     // Initialize display
-    lcd_init(LCD_DISP_ON_CURSOR_BLINK);
+    lcd_init(LCD_DISP_ON_CURSOR);
 
     // Put string(s) on LCD screen
-    lcd_gotoxy(6, 1);
+    lcd_gotoxy(4, 1);
     lcd_puts("LCD Test");
     lcd_putc('!');
 
     // Configuration of 8-bit Timer/Counter2 for Stopwatch update
     // Set the overflow prescaler to 16 ms and enable interrupt
+
+    TIM2_OVF_16MS
+    TIM2_OVF_ENABLE
+
+    // Custom character definition using https://omerk.github.io/lcdchargen/
+    uint8_t customChar[N_CHARS*8] = {
+        0b11111,
+        0b11011,
+        0b10001,
+        0b11011,
+        0b11011,
+        0b10001,
+        0b11011,
+        0b11111
+    };
+
+    lcd_command(1<<LCD_CGRAM);       // Set addressing to CGRAM (Character Generator RAM)
+                                     // ie to individual lines of character patterns
+    for (uint8_t i = 0; i < N_CHARS*8; i++)  // Copy new character patterns line by line to CGRAM
+        lcd_data(customChar[i]);
+    lcd_command(1<<LCD_DDRAM);       // Set addressing back to DDRAM (Display Data RAM)
+                                     // ie to character codes
+
+    // Display symbol with Character code 0
+    lcd_gotoxy(0, 1);
+    lcd_putc(0x00);
 
 
     // Enables interrupts by setting the global interrupt mask
@@ -81,21 +109,62 @@ ISR(TIMER2_OVF_vect)
 {
     static uint8_t no_of_overflows = 0;
     static uint8_t tenths = 0;  // Tenths of a second
-    char string[2];             // String for converted numbers by itoa()
+    static uint8_t sec = 0;  // Tenths of a second
+    static uint8_t min = 0;  // Tenths of a second
+    char string_tenths[2];   // String for converted numbers by itoa()
+    char string_sec[3];   // String for converted numbers by itoa()
+    char string_min[3];   // String for converted numbers by itoa()
 
     no_of_overflows++;
-    if (no_of_overflows >= 6)
+    if (no_of_overflows >= 1)
     {
         // Do this every 6 x 16 ms = 100 ms
         no_of_overflows = 0;
 
         // Count tenth of seconds 0, 1, ..., 9, 0, 1, ...
+        tenths++;
+        if (tenths > 9){
+            tenths = 0;
+            sec++;
+        }
+        if (sec > 59){
+            sec = 0;
+            min++;
+        }
 
+        if(min == 1 && sec == 10){
+            tenths = 0;
+            sec = 0;
+            min = 0;
+        }
 
-        itoa(tenths, string, 10);  // Convert decimal value to string
+        itoa(tenths, string_tenths, 10);  // Convert decimal value to string
+        itoa(sec, string_sec, 10);  // Convert decimal value to string
+        itoa(min, string_min, 10);  // Convert decimal value to string
         // Display "00:00.tenths"
+        if(min < 10){
+            lcd_gotoxy(5, 0);
+            lcd_puts("0");
+            lcd_gotoxy(6, 0);
+            lcd_puts(string_min);
+        }else{
+            lcd_gotoxy(6, 0);
+            lcd_puts(string_min);
+        }        
         lcd_gotoxy(7, 0);
-        lcd_puts(string);
+        lcd_puts(":");
+        if(sec < 10){
+            lcd_gotoxy(8, 0);
+            lcd_puts("0");
+            lcd_gotoxy(9, 0);
+            lcd_puts(string_sec);
+        }else{
+            lcd_gotoxy(8, 0);
+            lcd_puts(string_sec);
+        }
+        lcd_gotoxy(10, 0);
+        lcd_puts(".");
+        lcd_puts(string_tenths);
     }
     // Else do nothing and exit the ISR
 }
